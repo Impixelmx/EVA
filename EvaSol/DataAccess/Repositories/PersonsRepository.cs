1c8b20e07d5253c0dc1adaedf983ffd83e181a28
JairSalinas <bytestudiomx@gmail.com>
2020-12-18 11:15:14 -0600

    qwe

--- EvaSol/DataAccess/Repositories/PersonsRepository.cs
+++ EvaSol/DataAccess/Repositories/PersonsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,39 @@ using DataAccess.Contracts;
 using DataAccess.Entities;
 namespace DataAccess.Repositories
 {
-    public class PersonsRepository
+    public class PersonsRepository : MasterRepository, IPersonsRespository
     {
+        private string selectAll;
+        private string insert;
+        private string update;
+        private string delete;
 
+        public PersonsRepository()
+        {
+            selectAll= "SELECT * FROM table_name;";
+            insert = "INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'); ";
+            update = "UPDATE Customers SET ContactName = 'Alfred Schmidt', City = 'Frankfurt' WHERE CustomerID = 1; ";
+            delete = "DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';";
+        }
+        public Guid Add(Persons entity)
+        {
+            parameters = new List<SqlParameter>();
+            parameters.Add(new)
+        }
+
+        public Guid Adit(Persons entity)
+        {
+             
+        }
+
+        public IEnumerable<Persons> GetAll()
+        {
+             
+        }
+
+        public Guid Remove(Guid IdPersona)
+        {
+            
+        }
     }
 }
--- EvaSol/DataAccess/Repositories/Respository.cs
+++ EvaSol/DataAccess/Repositories/Respository.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace DataAccess.Repositories
 {
-    public class Respository
+    public abstract class Respository
     {
+        private readonly string connectionString;
+        public Respository()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Eva.Properties.Settings.EVA"].ToString();
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
     }
 }
